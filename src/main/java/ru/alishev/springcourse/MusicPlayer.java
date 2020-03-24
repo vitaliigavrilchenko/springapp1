58e3281eb056a0082adcb70e39eecaeb67bdf0b6
vitalii gavrilchenko <vitaliigavrilchenko@gmail.com>
2020-03-24 20:23:44 +0300

    di part 2

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -2,6 +2,24 @@ package ru.alishev.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
@@ -9,5 +27,12 @@ public class MusicPlayer {
 
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {
     }
 }
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -7,6 +7,8 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
