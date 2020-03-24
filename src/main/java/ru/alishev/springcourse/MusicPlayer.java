93fab040dd00127d31d236811862f0cf92327a83
vitalii gavrilchenko <vitaliigavrilchenko@gmail.com>
2020-03-24 20:51:13 +0300

    di homework (list injection)

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,7 +1,10 @@
 package ru.alishev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
@@ -13,6 +16,14 @@ public class MusicPlayer {
         this.name = name;
     }
 
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
     public int getVolume() {
         return volume;
     }
@@ -21,16 +32,10 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
+        for (Music music : musicList) {
+            System.out.println(music.getSong());
+        }
     }
 
     public MusicPlayer() {
