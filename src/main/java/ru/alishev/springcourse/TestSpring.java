762b5ff9017220ebbc91087f3a61bc3edd3ab743
vitalii gavrilchenko <vitaliigavrilchenko@gmail.com>
2020-03-25 16:06:03 +0300

    Bean lifecycle, init, destroy, factory method

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -6,7 +6,10 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
+        /*MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         boolean comparison = firstMusicPlayer == secondMusicPlayer;
@@ -19,7 +22,8 @@ public class TestSpring {
         firstMusicPlayer.setVolume(10);
 
         System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());*/
+
 
         /*System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());*/
