e5c1ea055d8caa47a0a0b824e446b47f02adfab6
vitalii gavrilchenko <vitaliigavrilchenko@gmail.com>
2020-03-25 18:58:26 +0300

    autowired

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -4,10 +4,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
+        /*Music music = context.getBean("classicalMusic", Music.class);
         MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
+
+        Music music2 = context.getBean("rockMusic", Music.class);
+        MusicPlayer rockMP = new MusicPlayer(music2);
+        rockMP.playMusic();
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusic();*/
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
+
         context.close();
     }
 }
